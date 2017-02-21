d20f53ff3006261de4f656ae500713ebb22d779b
Артем <Hippi_smartik1988>
2017-02-21 16:51:02 +0200

    testGUI

--- testGUI/src/testGUI/testGUI.java
+++ testGUI/src/testGUI/testGUI.java
@@ -4,11 +4,20 @@ import GUI.MyButton;
 import GUI.MyFrame;
 import GUI.MyPanel;
 
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 import java.awt.*;
 
 public class testGUI {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new MetalLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        JFrame.setDefaultLookAndFeelDecorated(true);
         //создаем кнопку
         MyButton button = new MyButton("Button");
         //вкладываем кнопку в панель
